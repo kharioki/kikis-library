588d91919be0ea47037637b7d5410058ebfa5909
kharioki <antolisah@gmail.com>
2020-02-27 23:25:11 +0300

    added my library books to collection

--- src/utils/library-data.js
+++ src/utils/library-data.js
@@ -1,64 +1,99 @@
-const LIBRARY_DATA = 
+const LIBRARY_DATA = {
+  id: 1,
+  title: "All Books",
+  items: [
     {
       id: 1,
-      title: 'All Books',
-      items: [
-        {
-          id: 1,
-          name: 'Brown Brim',
-          imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
-          price: 25
-        },
-        {
-          id: 2,
-          name: 'Blue Beanie',
-          imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
-          price: 18
-        },
-        {
-          id: 3,
-          name: 'Brown Cowboy',
-          imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
-          price: 35
-        },
-        {
-          id: 4,
-          name: 'Grey Brim',
-          imageUrl: 'https://i.ibb.co/RjBLWxB/grey-brim.png',
-          price: 25
-        },
-        {
-          id: 5,
-          name: 'Green Beanie',
-          imageUrl: 'https://i.ibb.co/YTjW3vF/green-beanie.png',
-          price: 18
-        },
-        {
-          id: 6,
-          name: 'Palm Tree Cap',
-          imageUrl: 'https://i.ibb.co/rKBDvJX/palm-tree-cap.png',
-          price: 14
-        },
-        {
-          id: 7,
-          name: 'Red Beanie',
-          imageUrl: 'https://i.ibb.co/bLB646Z/red-beanie.png',
-          price: 18
-        },
-        {
-          id: 8,
-          name: 'Wolf Cap',
-          imageUrl: 'https://i.ibb.co/1f2nWMM/wolf-cap.png',
-          price: 14
-        },
-        {
-          id: 9,
-          name: 'Blue Snapback',
-          imageUrl: 'https://i.ibb.co/X2VJP2W/blue-snapback.png',
-          price: 16
-        }
-      ]
-    };
-  
-  export default LIBRARY_DATA;
-  
+      name: "Sickened: how the government ruined healthcare",
+      imageUrl:
+        "https://images-na.ssl-images-amazon.com/images/I/91tlPoKBP7L.__BG0,0,0,0_FMpng_AC_UL720_SR720,720_.jpg",
+      type: "novel"
+    },
+    {
+      id: 2,
+      name: "Altered Carbon",
+      imageUrl:
+        "https://images-na.ssl-images-amazon.com/images/I/81XCMJkfg6L._AC_SX368_.jpg",
+      type: "fiction"
+    },
+    {
+      id: 3,
+      name: "Sun Tzu: The art of war",
+      imageUrl:
+        "https://images-na.ssl-images-amazon.com/images/I/81%2B9BJECh-L._AC_UL480_SR480,480_.jpg",
+      type: "regular"
+    },
+    {
+      id: 4,
+      name: "2020 Pizza cookbook: 100+ recipes",
+      imageUrl:
+        "https://images-na.ssl-images-amazon.com/images/I/71-7niWFo8L.__BG0,0,0,0_FMpng_AC_UL480_SR480,480_.jpg",
+      type: "novel"
+    },
+    {
+      id: 5,
+      name: "1984(Signet Classics)",
+      imageUrl:
+        "https://images-na.ssl-images-amazon.com/images/I/71-qZ2Z754L._AC_SX368_.jpg",
+      type: "fiction"
+    },
+    {
+      id: 6,
+      name: "A brief history of time",
+      imageUrl:
+        "https://images-na.ssl-images-amazon.com/images/I/A1xkFZX5k-L._AC_SX368_.jpg",
+      type: "novel"
+    },
+    {
+      id: 7,
+      name: "The right stuff",
+      imageUrl:
+        "https://images-na.ssl-images-amazon.com/images/I/51ZgqYqwuAL._AC_SX368_.jpg",
+      type: "novel"
+    },
+    {
+      id: 8,
+      name: "Game of Thrones: A song of fire and ice",
+      imageUrl:
+        "https://images-na.ssl-images-amazon.com/images/I/416EPXkwv8L._AC_SX368_.jpg",
+      type: "fiction"
+    },
+    {
+      id: 9,
+      name: "American Gods",
+      imageUrl:
+        "https://images-na.ssl-images-amazon.com/images/I/51GcnFtO1+L._AC_SX368_.jpg",
+      type: "fiction"
+    },
+    {
+      id: 10,
+      name: "Steve jobs",
+      imageUrl:
+        "https://images-na.ssl-images-amazon.com/images/I/81VStYnDGrL._AC_SX368_.jpg",
+      type: "regular"
+    },
+    {
+      id: 11,
+      name: "It doesn't have to be crazy at work",
+      imageUrl:
+        "https://m.media-amazon.com/images/I/81nvF-p7odL._AC_UY218_ML3_.jpg",
+      type: "regular"
+    },
+    {
+      id: 12,
+      name: "Rework",
+      imageUrl:
+        "https://m.media-amazon.com/images/I/71SqvmjaM3L._AC_UY218_ML3_.jpg",
+      type: "regular"
+    },
+    {
+      id: 13,
+      name: "Remote",
+      imageUrl:
+        "https://m.media-amazon.com/images/I/71oKgphAjyL._AC_UY218_ML3_.jpg",
+      type: "regular"
+    }
+  ]
+};
+
+export default LIBRARY_DATA;
