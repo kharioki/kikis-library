be5820490e5b71d5af6ece980606337831cac087
kharioki <antolisah@gmail.com>
2020-03-02 14:51:13 +0300

    updating app to be pwa compliant, updated app assets, added service worker, https, updates manifest

--- client/src/components/header/header.component.jsx
+++ client/src/components/header/header.component.jsx
@@ -2,15 +2,15 @@ import React, { useState } from "react";
 
 import "./header.styles.scss";
 
-import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
 import CartIconContainer from "../cart-icon/cart-icon.container";
 import CartDropdownContainer from "../cart-dropdown/cart-dropdown.container";
 
 const Header = () => {
-  const [hidden, setHidden] = useState(false);
+  const [hidden, setHidden] = useState(true);
   return (
     <div className="header">
-      <Logo className="logo" />
+      <Logo className="logo-container" />
       <div className="options">
         <CartIconContainer onClick={() => setHidden(!hidden)} />
       </div>
